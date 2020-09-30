a6e5c6d9350c99d59e05be03c7578f19c8ef3f56
Rafael Schwan <rafaelschwan@hotmail.com>
2020-09-30 08:47:57 -0300

    Desenvolvida a seção de detalhes e finalização da estilização

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
 
 import { Title, Form, Repositories, Error } from './styles';
-import Repository from '../Repository';
 
 interface Repository {
   full_name: string;
@@ -71,16 +71,18 @@ const Dashboard: React.FC = () => {
 
         <Repositories>
           {repositories.map(repository => (
-            <a key={repository.full_name} href="teste">
-            <img src={repository.owner.avatar_url} alt={repository.owner.login} />
-            <div>
-              <strong>{repository.full_name}</strong>
-              <p>{repository.description}</p>
-            </div>
-            <FiChevronRight size={20} />
-            </a>
+            <Link key={repository.full_name} to={`/repositories/${repository.full_name}`}>
+              <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+              <div>
+                <strong>{repository.full_name}</strong>
+                <p>{repository.description}</p>
+              </div>
+              <FiChevronRight size={20} />
+            </Link>
           ))}
         </Repositories>
+
+
 
       </>
     );
